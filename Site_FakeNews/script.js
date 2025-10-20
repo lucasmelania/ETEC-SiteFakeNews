64b3959d6babb78a2c45dc933b0a96bf25f421d2
Lucas Lopes Melania <contact.lucasmelania@gmail.com>
2025-10-20 10:55:40 -0300

    Mudança site
    incrementei o bootstrap

--- Site_FakeNews/script.js
+++ Site_FakeNews/script.js
@@ -1,32 +1,31 @@
-const openButton = document.getElementById('open-sidebar-button')
-const navbar = document.getElementById('navbar')
+const openButton = document.getElementById("open-sidebar-button");
+const navbar = document.getElementById("Mynavbar");
 
-const media = window.matchMedia("(width < 700px)")
+const media = window.matchMedia("(width < 700px)");
 
-media.addEventListener('change', (e) => updateNavbar(e))
+media.addEventListener("change", (e) => updateNavbar(e));
 
-function updateNavbar(e){
-  const isMobile = e.matches
-  console.log(isMobile)
-  if(isMobile){
-    navbar.setAttribute('inert', '')
-  }
-  else{
+function updateNavbar(e) {
+  const isMobile = e.matches;
+  console.log(isMobile);
+  if (isMobile) {
+    navbar.setAttribute("inert", "");
+  } else {
     // desktop device
-    navbar.removeAttribute('inert')
+    navbar.removeAttribute("inert");
   }
 }
 
-function openSidebar(){
-  navbar.classList.add('show')
-  openButton.setAttribute('aria-expanded', 'true')
-  navbar.removeAttribute('inert')
+function openSidebar() {
+  navbar.classList.add("show");
+  openButton.setAttribute("aria-expanded", "true");
+  navbar.removeAttribute("inert");
 }
 
-function closeSidebar(){
-  navbar.classList.remove('show')
-  openButton.setAttribute('aria-expanded', 'false')
-  navbar.setAttribute('inert', '')
+function closeSidebar() {
+  navbar.classList.remove("show");
+  openButton.setAttribute("aria-expanded", "false");
+  navbar.setAttribute("inert", "");
 }
 
 // For Bookmark Links
@@ -37,4 +36,4 @@ function closeSidebar(){
 //   })
 // })
 
-updateNavbar(media)
+updateNavbar(media);
